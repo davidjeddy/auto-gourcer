8750616d459505bcfdb89cb44a3a6dc7a82a6a3e
David J Eddy <me@davidjeddy.com>
2017-09-12 13:07:45 -0400

    Work conts on the php logic. Working proto-type functional

---
+++ src/BitBucket.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+namespace davidjeddy\AutoGourcer;
+
+/**
+ * Class BitBucket
+ *
+ * Use this to interface with BitBucket SVN service
+ *
+ * @package davidjeddy\AutoGourcer
+ */
+class BitBucket
+{
+
+}
