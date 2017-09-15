482973ca3d138764615441666bc076676f86d6f5
David J Eddy <me@davidjeddy.com>
2017-09-15 12:00:03 -0400

    CRC

--- src/Git.php
+++ src/Git.php
@@ -46,24 +46,39 @@ class Git
      * @param string $branch
      * @return Git
      */
-    public function checkout($path = null, $branch = 'latest'): self
+    public function checkout($path = null, $branch = null): self
     {
-        if ($branch === 'latest') {
-            // @source https://gist.github.com/jasonrudolph/1810768
-            \exec ('cd ' . $path . ' && for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\ $branch; done | sort -r', $responseData, $errorCode);
-
-            // parse the string and get the latest branch text
-            $branch = $responseData[0];
-            $branch = \explode(' ', $branch);
-            $branch = $branch[\count($branch) - 1];
-            $branch = \explode('/', $branch);
-            $branch = $branch[\count($branch) - 1];
+        if ($branch === null) {
+            $branch = $this->checkoutLatestchanges($path);
         }
 
-        // exec git command
         echo ("Checking out branch {$branch} of repo {$path} .\n");
         \exec ("cd {$path} && git checkout {$branch} 2>> /auto_gourcer/logs/git.log");
 
         return $this;
+    }
+
+    // private methods
+
+    /**
+     * This is a bit convoluted: Change to repo dir. print reflog, get hash of most recent entry, return hash string.
+     *
+     * @source https://gist.github.com/jasonrudolph/1810768
+     * @fork https://gist.github.com/davidjeddy/2b5d9362fb0728a53977364b2a22ab44
+     * @param string $path
+     * @return string
+     */
+    private function checkoutLatestChanges(string $path): string
+    {
+        \exec ('cd ' . $path . ' && for branch in `git branch -r | grep -v HEAD`;do echo -e `git show --format="%ci %cr" $branch | head -n 1` \\ $branch; done | sort -r', $responseData, $errorCode);
+
+        // parse the string and get the latest branch text
+        $branch = $responseData[0];
+        $branch = \explode(' ', $branch);
+        $branch = $branch[\count($branch) - 1];
+        $branch = \explode('/', $branch);
+        $branch = $branch[\count($branch) - 1];
+
+        return $branch;
     }
 }
