c413c381616943716747e9a6eb9d595629d12561
David J Eddy <me@davidjeddy.com>
2017-09-22 12:08:31 -0400

    UPDATED to readme.md about tracking ./run.php

--- run.php
+++ run.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-// NOTE: no changes to this file are tracked unless GiT's --no-assume-unchanged is set.
 // @source https://stackoverflow.com/questions/9794931/keep-file-in-a-git-repo-but-dont-track-changes
 // if vendors dir does not exist, OR day of mont is 14; get composer.phar
 if (!file_exists('./vendor') || !file_exists('./composer.phar') || date('d') == 14) {
@@ -32,7 +31,7 @@ class Run
     {
         // At the very least we need Git credentials in order to access BitBucket repositories.
         $gitClass = new Git();
-        $gitClass->setHost('bitbucket.org')->setUser('David_Eddy')->setPass('~Celcerzero7')->setOrg('Sourcetoad');
+        $gitClass->setUser('')->setPass('')->setOrg('');
 
         // Here we override some of the Gource class properties
         $gourceClass = new Gource();
