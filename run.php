701d95955713f4af82afb5ac08074e0394865ca7
David J Eddy <me@davidjeddy.com>
2017-10-12 17:45:20 -0400

    ADDED remote host BitBucket ITERATED on run.php

--- run.php
+++ run.php
@@ -15,10 +15,12 @@ include_once '/auto-gourcer/vendor/autoload.php';
 include_once '/auto-gourcer/src/AutoGourcer.php';
 include_once '/auto-gourcer/src/Git.php';
 include_once '/auto-gourcer/src/Gource.php';
+include_once '/auto-gourcer/src/RemoteHost/BitBucket.php';
 
 use \dje\AutoGourcer\AutoGourcer;
 use \dje\AutoGourcer\Git;
 use \dje\AutoGourcer\Gource;
+use \dje\AutoGourcer\RemoteHost\BitBucket;
 
 /**
  *
@@ -30,17 +32,23 @@ class Run
      */
     public static function program()
     {
-        // At the very least we need Git credentials in order to access BitBucket repositories.
-        $gitClass = new Git();
-        $gitClass->setHost('bitbucket.org')->setUser('')->setPass('')->setOrg('');
+        // Configure cred's for remote host
+        $remoteHost = new BitBucket();
+        $remoteHost->setUser('')->setPass('')->setOrg('');
+
+        // Using remote host, GiT repos
+        $gitClass = new Git($remoteHost);
+        $gitClass->setRepoCount(5)->getRepos(); // not sure if we want to keep this or auto-trigger 'getRepos()'.
+
 
         // Here we override some of the Gource class properties
         $gourceClass = new Gource();
-        $gourceClass->setFramerate(30);
+        $gourceClass->setFramerate(30)->setStartDate('2017-10-11');
 
         // Override the AutoGourcer defaults
         $ag = new AutoGourcer();
-        $ag->setRepoCount(5)->setGit($gitClass)->setGource($gourceClass);
+        $ag->setGit($gitClass)->setGource($gourceClass);
+
 
         // Finally run the process
         $ag->run();
